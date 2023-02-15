c0124e79b2557baa5f984c472f54d31232375ed2
GRAY_KO_PC\gray.ko <gray.ko@spintech.co.kr>
2023-02-15 15:17:17 +0900

    first commit

---
+++ src/main/java/com/example/test1/vdpdemo/VdpdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.test1.vdpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdpdemoApplication.class, args);
+	}
+
+}
